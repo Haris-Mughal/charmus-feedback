a237bd67c371d2fddc00973082dd70dbc5b70a96
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2024-06-22 16:16:22 +0500

    simple text on / route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>
-      <></>
-    </>
+    <div className="flex justify-center items-center py-[17%] ">
+      <h1 className="text-[48px]">Charmus Message</h1>
+    </div>
   );
 }
