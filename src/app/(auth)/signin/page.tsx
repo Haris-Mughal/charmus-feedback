e779f20fb28a9fb33b504a6ef220d466fdef8303
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2024-06-24 21:19:00 +0500

    shadcn/ui forms | toast components initialized

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,16 +1,8 @@
+"use client";
 import React from "react";
 
 function page() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg text-center">
-        <h1 className="text-2xl text-black font-bold mb-4">Sign In Page</h1>
-        <p className="text-gray-600">
-          Enter your email and password to sign in
-        </p>
-      </div>
-    </div>
-  );
+  return <></>;
 }
 
 export default page;
