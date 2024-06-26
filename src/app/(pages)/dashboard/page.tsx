acd641141dff533727df5a7d8c513e816be68f6c
Muhammad Haris Ahsan <harismirza3456@gmail.com>
2024-06-27 01:27:02 +0500

    new routes added

---
+++ src/app/(pages)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="flex justify-center items-center py-[17%] ">
+      <h1 className="text-[48px]">Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/app/m/[username]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Fuck = () => {
+  return (
+    <div className="flex justify-center items-center py-[17%] ">
+      <h1 className="text-[48px]">Fuck u</h1>
+    </div>
+  );
+};
+
+export default Fuck;
